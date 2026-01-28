9522fa3cc0426cfb23d68d6a926ba564b88b3539
Melody-Lin <yiihsuanlin@gmail.com>
2026-01-27 20:36:49 -0800

    exercise: 03-web-base

--- src/app/03-web-base/SiteFooter.js
+++ src/app/03-web-base/SiteFooter.js
@@ -3,58 +3,59 @@ import Link from 'next/link';
 
 import { getNavLinks } from '@/helpers/web-base-helpers';
 
-async function SiteFooter() {
-  const navLinks = await getNavLinks();
-
+function SiteFooter() {
   return (
-    <footer className="site-footer">
-      <div className="logo-wrapper">
-        <Link href="" className="logo">
+    <footer className='site-footer'>
+      <div className='logo-wrapper'>
+        <Link href='' className='logo'>
           Webzip
         </Link>
-        <p className="disclaimer">
-          Copyright © 2099 Webzip Inc. All Rights
-          Reserved.
+        <p className='disclaimer'>
+          Copyright © 2099 Webzip Inc. All Rights Reserved.
         </p>
       </div>
 
-      <div className="link-wrapper">
-        <div className="col">
+      <div className='link-wrapper'>
+        <div className='col'>
           <h2>Navigation</h2>
           <nav>
-            <ol>
-              {navLinks.map(
-                ({ slug, label, href }) => (
-                  <li key={slug}>
-                    <Link href={href}>
-                      {label}
-                    </Link>
-                  </li>
-                )
-              )}
-            </ol>
+            <React.Suspense>
+              <PrimaryNavLinks />
+            </React.Suspense>
           </nav>
         </div>
-        <div className="col">
+        <div className='col'>
           <h2>Legal</h2>
           <nav>
             <ol>
               <li>
-                <Link href="">Terms of Use</Link>
+                <Link href=''>Terms of Use</Link>
               </li>
               <li>
-                <Link href="">
-                  Privacy Policy
-                </Link>
+                <Link href=''>Privacy Policy</Link>
               </li>
               <li>
-                <Link href="">Contact</Link>
+                <Link href=''>Contact</Link>
               </li>
             </ol>
           </nav>
         </div>
       </div>
     </footer>
+  );
+}
+
+async function PrimaryNavLinks() {
+  const navLinks = await getNavLinks();
+
+  return (
+    <ol>
+      {navLinks.map(({ slug, label, href }) => (
+        <li key={slug}>
+          <Link href={href}>{label}</Link>
+        </li>
+      ))}
+    </ol>
   );
 }
 
--- src/app/03-web-base/SiteHeader.js
+++ src/app/03-web-base/SiteHeader.js
@@ -3,35 +3,37 @@ import Link from 'next/link';
 
 import { getNavLinks } from '@/helpers/web-base-helpers';
 
-async function SiteHeader() {
+function SiteHeader() {
+  return (
+    <header className='site-header'>
+      <Link href='' className='logo'>
+        WebBase
+      </Link>
+      <nav>
+        <React.Suspense>
+          <NavLinks />
+        </React.Suspense>
+      </nav>
+    </header>
+  );
+}
+
+async function NavLinks() {
   let navLinks = await getNavLinks();
 
   // Only show the first 4 links in the header.
   navLinks = navLinks.slice(0, 4);
 
   return (
-    <header className="site-header">
-      <Link href="" className="logo">
-        WebBase
-      </Link>
-      <nav>
-        <ol className="header-nav-links">
-          {navLinks.map(
-            ({ slug, label, href, type }) => (
-              <li key={slug}>
-                <Link
-                  href={href}
-                  className={`header-nav-link ${type}`}
-                >
-                  {label}
-                </Link>
-              </li>
-            )
-          )}
-        </ol>
-      </nav>
-    </header>
+    <ol className='header-nav-links'>
+      {navLinks.map(({ slug, label, href, type }) => (
+        <li key={slug}>
+          <Link href={href} className={`header-nav-link ${type}`}>
+            {label}
+          </Link>
+        </li>
+      ))}
+    </ol>
   );
 }
-
 export default SiteHeader;
